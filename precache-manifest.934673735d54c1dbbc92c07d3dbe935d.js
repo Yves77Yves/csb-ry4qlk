617989a4911ad4a2778eab5f625dada09e2b487d
Code Sandbox <hello@codesandbox.io>
2022-08-11 14:02:17 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.934673735d54c1dbbc92c07d3dbe935d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3631c03cafbfb68a5c6c8535c79ff4ee",
+    "url": "https://yves77yves.github.io/csb-ry4qlk/index.html"
+  },
+  {
+    "revision": "33ad2cf6984f2abce024",
+    "url": "https://yves77yves.github.io/csb-ry4qlk/static/js/2.79aacc08.chunk.js"
+  },
+  {
+    "revision": "47d734c5a563c3060250",
+    "url": "https://yves77yves.github.io/csb-ry4qlk/static/js/main.31247fcd.chunk.js"
+  },
+  {
+    "revision": "9acc710642f3d65802f5",
+    "url": "https://yves77yves.github.io/csb-ry4qlk/static/js/runtime-main.e8c10fb9.js"
+  }
+]);
